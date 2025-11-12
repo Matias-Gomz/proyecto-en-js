06d5eddc8e1149009e2b6c3eae1c27dd9c9e29ce
Matias Gomez <matiasgomz.006@gmail.com>
2025-11-12 11:31:57 -0300

    Create testeo.js

---
+++ testeo.js
@@ -0,0 +1,47 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use((req, res, next) => {
+  console.log(`Middleware => ${req.method} ${req.url}`);
+  next();
+});
+
+
+// Datos de ejemplo (simulando una base de datos)
+const users = [
+    { id: 1, name: 'ana' },
+    { id: 2, name: 'Luis' },
+    { id: 3, name: 'Carla' },
+  ];
+
+// 5️⃣ Ruta raíz "/"
+// app.get() crea una ruta que responde a solicitudes GET.
+app.get('/', (req, res) => {
+  res.send('Welcome to Express.js!');
+});
+
+// 6️⃣ Ruta GET /users
+// Devolvemos un JSON de usuarios (como haría una API real)
+app.get('/users', (req, res) => {
+  res.json(users); // Express convierte automáticamente el objeto a JSON
+});
+
+//ruta users
+
+app.post('/users', (req, res) => {
+  const newUser = req.body; // Express ya parseó el JSON del body.
+  console.log('Nuevo usuario recibido:', newUser);
+
+  newUser.id = Math.floor(Math.random() * 1000);
+  res.status(201).json(newUser);
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Servidor Express corriendo en http://localhost:${PORT}`);
+});
